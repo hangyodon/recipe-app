66e93bbea1a7dfc491adcd1b730193ebc2b6b76b
hangyodon <64734488+hangyodon@users.noreply.github.com>
2020-06-12 21:46:15 +0900

    投稿時の画像プレビュー 追加

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,31 +1,161 @@
-// var input = document.querySelector('input[name=tags]'),
-//     // init Tagify script on the above inputs
-//     tagify = new Tagify(input, {
-//         whitelist : ["A# .NET", "A# (Axiom)"],
-//         blacklist : ["react", "angular"]
-//     });
-// // "remove all tags" button event listener
-// document.querySelector('.tags--removeAllBtn')
-//     .addEventListener('click', tagify.removeAllTags.bind(tagify))
-// // Chainable event listeners
-// tagify.on('add', onAddTag)
-//       .on('remove', onRemoveTag)
-//       .on('invalid', onInvalidTag);
-// // tag added callback
-// function onAddTag(e){
-//     console.log(e, e.detail);
-//     console.log( tagify.DOM.originalInput.value )
-//     tagify.off('add', onAddTag) // exmaple of removing a custom Tagify event
-// }
-// // tag remvoed callback
-// function onRemoveTag(e){
-//     console.log(e, e.detail);
-// }
-// // invalid tag added callback
-// function onInvalidTag(e){
-//     console.log(e, e.detail);
-// }
+$(document).on('turbolinks:load', function(){
+  $(function(){
 
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+  $('#recipe_image').change(function(){
+    //選択したfileのオブジェクトをpropで取得
+    var file = $('input[type="file"]').prop('files')[0];
+    //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+    var fileReader = new FileReader();
+    //読み込みが完了すると、srcにfileのURLを格納
+    fileReader.onloadend = function() {
+      var src = fileReader.result
+      var html= `<div class="preview-content">
+                   <div class="image-content">            
+                     <img src="${src}" >
+                   </div>
+                   <div class="lower-box">
+                      <div class="delete-box" id="delete_btn">
+                        削除する
+                      </div>
+                   </div>
+                 </div>`
+      //image_box__container要素の前にhtmlを差し込む
+      $('.preview-content').remove();
+      $('.img-field').before(html);
+    }
+    fileReader.readAsDataURL(file);
+
+    $(document).on('click', '.delete-box', function() {
+      $('.preview-content').remove();
+      $('#recipe_image').val("");
+
+    });
+
+    // $(document).on('click', '.update-box', function() {
+    //   $('#recipe_image').trigger("click")
+    //   $('.preview-content').remove();
+    // });
+    
+  });
+
+
+});
+});
+
+
+
+
+
+
+// $(document).on('turbolinks:load', function(){
+//   $(function(){
+//     function buildHTML(count) {
+//       var html = `<div class="preview-box" id="preview-box__${count}">
+//                     <div class="upper-box">
+//                       <img src="" alt="preview">
+//                     </div>
+//                     <div class="lower-box">
+//                       <div class="update-box" id="edit_btn_${count}">
+//                         編集
+//                       </div>
+//                       <div class="delete-box" id="delete_btn_${count}">
+//                         削除
+//                       </div>
+//                     </div>
+//                   </div>`
+//       return html;
+//     }
+
+//      // 投稿編集時
+//     //items/:i/editページへリンクした際のアクション=======================================
+//     if (window.location.href.match(/\/items\/\d+\/edit/)){
+//       //登録済み画像のプレビュー表示欄の要素を取得する
+//       var prevContent = $('.label-content').prev();
+//       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+//       $('.label-content').css('width', labelWidth);
+//       //プレビューにidを追加
+//       $('.preview-box').each(function(index, box){
+//         $(box).attr('id', `preview-box__${index}`);
+//       })
+//       //削除ボタンにidを追加
+//       $('.delete-box').each(function(index, box){
+//         $(box).attr('id', `delete_btn_${index}`);
+//       })
+//       $('.update-box').each(function(index, box){
+//         $(box).attr('id', `_btn_${index}`);
+//       })
+
+//       var count = $('.preview-box').length;
+//       //プレビューが5あるときは、投稿ボックスを消しておく
+//       if (count == 5) {
+//         $('.label-content').hide();
+//       }
+//     }
+//     //=============================================================================
+
+//     function setLabel() {
+//       var prevContent = $('.label-content').prev();
+//       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+//       $('.label-content').css('width', labelWidth);
+//     }
+
+
+//     $(document).on('change', '.hidden-field', function() {
+//       setLabel();
+//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
+//       $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_url`});
+//       var file = this.files[0];
+//       var reader = new FileReader();
+//       reader.readAsDataURL(file);
+//       reader.onload = function() {
+//         var image = this.result;
+
+//         if ($(`#preview-box__${id}`).length == 0) {
+//           var count = $('.preview-box').length;
+//           var html = buildHTML(id);
+//           var prevContent = $('.label-content').prev();
+//           $(prevContent).append(html);
+//         }
+
+//         $(`#preview-box__${id} img`).attr('src', `${image}`);
+//         var count = $('.preview-box').length;
+//         if (count == 5) { 
+//           $('.label-content').hide();
+//         }
+
+//         setLabel();
+//         if(count < 5){
+//           $('.label-box').attr({id: `label-box--${count}`,for: `item_images_attributes_${count}_url`});
+//         }
+//       }
+//     });
+//     $(document).on('click', '.delete-box', function() {
+//       var count = $('.preview-box').length;
+//       setLabel(count);
+//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
+//       $(`#preview-box__${id}`).remove();
+//       $(`#item_images_attributes_${id}_url`).val("");
+//       var count = $('.preview-box').length;
+//       if (count == 4) {
+//         $('.label-content').show();
+//       }
+//       setLabel(count);
+
+//       if(id < 5){
+//         $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_url`});
+//       }
+//     });
+
+//     $(document).on('click', '.update-box', function() {
+//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
+//       $(`#item_images_attributes_${id}_url`).trigger("click")
+
+//     })
+//   });
+// });
 
 
 
