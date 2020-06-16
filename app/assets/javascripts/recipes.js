c123a86e5a70c8fc288cda4196a89b6e92805692
hangyodon <64734488+hangyodon@users.noreply.github.com>
2020-06-16 22:00:16 +0900

    プルダウンメニュー実装

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -45,117 +45,12 @@ $(document).on('turbolinks:load', function(){
 });
 });
 
-
-
-
-
-
-// $(document).on('turbolinks:load', function(){
-//   $(function(){
-//     function buildHTML(count) {
-//       var html = `<div class="preview-box" id="preview-box__${count}">
-//                     <div class="upper-box">
-//                       <img src="" alt="preview">
-//                     </div>
-//                     <div class="lower-box">
-//                       <div class="update-box" id="edit_btn_${count}">
-//                         編集
-//                       </div>
-//                       <div class="delete-box" id="delete_btn_${count}">
-//                         削除
-//                       </div>
-//                     </div>
-//                   </div>`
-//       return html;
-//     }
-
-//      // 投稿編集時
-//     //items/:i/editページへリンクした際のアクション=======================================
-//     if (window.location.href.match(/\/items\/\d+\/edit/)){
-//       //登録済み画像のプレビュー表示欄の要素を取得する
-//       var prevContent = $('.label-content').prev();
-//       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
-//       $('.label-content').css('width', labelWidth);
-//       //プレビューにidを追加
-//       $('.preview-box').each(function(index, box){
-//         $(box).attr('id', `preview-box__${index}`);
-//       })
-//       //削除ボタンにidを追加
-//       $('.delete-box').each(function(index, box){
-//         $(box).attr('id', `delete_btn_${index}`);
-//       })
-//       $('.update-box').each(function(index, box){
-//         $(box).attr('id', `_btn_${index}`);
-//       })
-
-//       var count = $('.preview-box').length;
-//       //プレビューが5あるときは、投稿ボックスを消しておく
-//       if (count == 5) {
-//         $('.label-content').hide();
-//       }
-//     }
-//     //=============================================================================
-
-//     function setLabel() {
-//       var prevContent = $('.label-content').prev();
-//       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
-//       $('.label-content').css('width', labelWidth);
-//     }
-
-
-//     $(document).on('change', '.hidden-field', function() {
-//       setLabel();
-//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-//       $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_url`});
-//       var file = this.files[0];
-//       var reader = new FileReader();
-//       reader.readAsDataURL(file);
-//       reader.onload = function() {
-//         var image = this.result;
-
-//         if ($(`#preview-box__${id}`).length == 0) {
-//           var count = $('.preview-box').length;
-//           var html = buildHTML(id);
-//           var prevContent = $('.label-content').prev();
-//           $(prevContent).append(html);
-//         }
-
-//         $(`#preview-box__${id} img`).attr('src', `${image}`);
-//         var count = $('.preview-box').length;
-//         if (count == 5) { 
-//           $('.label-content').hide();
-//         }
-
-//         setLabel();
-//         if(count < 5){
-//           $('.label-box').attr({id: `label-box--${count}`,for: `item_images_attributes_${count}_url`});
-//         }
-//       }
-//     });
-//     $(document).on('click', '.delete-box', function() {
-//       var count = $('.preview-box').length;
-//       setLabel(count);
-//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-//       $(`#preview-box__${id}`).remove();
-//       $(`#item_images_attributes_${id}_url`).val("");
-//       var count = $('.preview-box').length;
-//       if (count == 4) {
-//         $('.label-content').show();
-//       }
-//       setLabel(count);
-
-//       if(id < 5){
-//         $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_url`});
-//       }
-//     });
-
-//     $(document).on('click', '.update-box', function() {
-//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-//       $(`#item_images_attributes_${id}_url`).trigger("click")
-
-//     })
-//   });
-// });
-
-
-
+$(document).on('turbolinks:load', function(){
+$(function(){
+    $('.dropdwn li').hover(function(){
+        $("ul", this).slideDown();
+    }, function(){
+        $("ul.dropdwn_menu",this).slideUp();
+    });
+  });
+});
